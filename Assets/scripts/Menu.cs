c08c0f04d52ae9bb967e0e05e1eaaa28a05fe898
Evdokimov27 <mr.evdokimov27@yandex.ru>
2022-02-21 22:49:01 +0800

    upd shop

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject GameMenu;
+    [SerializeField] private GameObject UI;
+    [SerializeField] private GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    public void Play()
+    {
+        GameMenu.SetActive(false);
+        Player.SetActive(true);
+        UI.SetActive(true);
+    }
+    public void Shop()
+    {
+        GameMenu.SetActive(true);
+        UI.SetActive(false);
+        
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
