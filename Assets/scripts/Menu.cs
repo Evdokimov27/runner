cae8c46ed4917eb85bb5a7d97bbd946676456f6f
Evdokimov27 <mr.evdokimov27@yandex.ru>
2022-02-22 23:34:10 +0800

    upd map

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Menu : MonoBehaviour
     [SerializeField] private GameObject GameMenu;
     [SerializeField] private GameObject UI;
     [SerializeField] private GameObject Player;
+    [SerializeField] private Text coinsText;
+    [SerializeField] private int coins;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,8 @@ public class Menu : MonoBehaviour
     }
     public void Play()
     {
-      
+        coins = PlayerPrefs.GetInt("coins");
+        coinsText.text = coins.ToString(); 
         GameMenu.SetActive(false);
         Player.SetActive(true);
         UI.SetActive(true);
