ebe993485c3997a07b7d09a7422840dc8611f3ed
Evdokimov27 <mr.evdokimov27@yandex.ru>
2022-02-26 03:35:32 +0800

    refactoring Skins.cs

--- Assets/scripts/Skins.cs
+++ Assets/scripts/Skins.cs
@@ -4,37 +4,23 @@ using System.Collections;
 public class Skins : MonoBehaviour
 {
     Animator avatar;
-    public Avatar skin1;
-    public Avatar skin2;
-    public Avatar skin3;
-    public Avatar skin4;
-    [SerializeField] public GameObject Player;
+    public Avatar[] skins;
+    [SerializeField] private GameObject Player;
 
     GameObject model;
     // Use this for initialization
     private void Awake()
     {
         int skin = PlayerPrefs.GetInt("skin");
-        if (skin == 0)
+        for (int x = 0; x < skins.Length; x++)
         {
-            avatar = GetComponent<Animator>();
-            avatar.avatar = skin1 as Avatar;
+            if (x == skin)
+            {
+                avatar = GetComponent<Animator>();
+                avatar.avatar = skins[skin] as Avatar;
+            }
         }
-        if (skin == 1)
-        {
-            avatar = GetComponent<Animator>();
-            avatar.avatar = skin2 as Avatar;
-        }
-        if (skin == 2)
-        {
-            avatar = GetComponent<Animator>();
-            avatar.avatar = skin3 as Avatar;
-        }
-        if (skin == 3)
-        {
-            avatar = GetComponent<Animator>();
-            avatar.avatar = skin4 as Avatar;
-        }
+       
 
         // Update is called once per frame
 
